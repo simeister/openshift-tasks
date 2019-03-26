1447cbc38c4e4dd4223a036f61e9cea967496bcc
simone meister <simone.meister-devoteam.com@bastion.0a1a.example.opentlc.com>
2019-03-26 08:25:13 +0000

    homework build pipeline

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -22,7 +22,7 @@ oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 # TBD
-oc new-app https://github.com/simeister/openshift-tasks \ --context-dir=openshift-tasks -n ${GUID}-jenkins
+oc new-app https://github.com/simeister/openshift-tasks --context-dir=openshift-tasks -n ${GUID}-jenkins
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
   echo "Checking if Jenkins is Ready..."
